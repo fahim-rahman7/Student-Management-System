a1e49b869089a491267cabfd8724c93fe03db5f4
Fahim Rahman <fahimrahmandipto@gmail.com>
2026-07-21 04:24:58 +0600

    initial commit

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,8 +2,10 @@ const express = require("express");
 const router = express.Router();
 
 const auth = require("./auth.js");
+const admin = require("./admin.js");
 
 router.use("/auth", auth);
+router.use("/admin", admin);
 
 
 
